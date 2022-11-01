d12fc33af3184a9f6ba9898366ad9b6131216e36
Paul Catana <paulcatana97@yahoo.com>
2022-11-01 12:09:32 +0200

    Implemented ContainsLetters function and DoesNotContainLetters test.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,12 +6,26 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return IsZero(input);
+            return IsZero(input)
+                && !ContainsLetters(input);
         }
 
         static bool IsZero(string input)
         {
             return input == "0";
+        }
+
+        static bool ContainsLetters(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] > 'a' && input[i] < 'z') && (input[i] < 'A' || input[i] > 'Z'))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
