ab6242706969fef23cad9b664f5c2539524c2a76
Paul Catana <paulcatana97@yahoo.com>
2022-12-12 16:12:50 +0200

    implemented Range Class and Unit Tests

---
+++ problema 3/problema_3/problema_3/Range.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace problema_3
+{
+    public class Range
+    {
+        private char start;
+        private char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            return (!string.IsNullOrEmpty(text)) && (text[0] >= start && text[0] <= end);
+        }
+    }
+}
