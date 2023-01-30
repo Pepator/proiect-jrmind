58bd0254fe6ebd89b7cb6ae2c4ac4dce4b5cbb58
Paul Catana <paulcatana97@yahoo.com>
2023-01-30 06:22:47 +0200

    Implemented Any class and Any UnitTests.

---
+++ problema 3/Problema_3/Problema_3/Any.cs
@@ -0,0 +1,29 @@
+﻿using problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class Any : IPattern
+    {
+        private string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text) || !accepted.Contains(text[0]))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(true, text[1..]);
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnitTest_Any.cs
@@ -0,0 +1,47 @@
+﻿using Problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace problema_3
+{
+    public class UnitTest_Any
+    {
+        [Fact]
+        public void Match_ValidateAnyClass_ShouldRetrunTrueAndRemainingTextIfConditionsAreMet()
+        {
+            var e = new Problema_3.Any("eE");
+            var match = e.Match("ea");
+            Assert.Equal((true, "a"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateAnyClass_ShouldRetrunFalseAndText()
+        {
+            var e = new Problema_3.Any("eE");
+            var match = e.Match("aa");
+            Assert.Equal((false, "aa"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateAnyClass_ShouldRetrunTrueAndRemainingTextIfConditionsAreMetForSignCharacters()
+        {
+            var sign = new Problema_3.Any("-+");
+            var match = sign.Match("+aa");
+            Assert.Equal((true, "aa"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateAnyClass_ShouldRetrunfalseAndNull()
+        {
+            var sign = new Problema_3.Any("-+");
+            var match = sign.Match(null);
+            Assert.Equal((false, null), (match.Succes(), match.RemainingText()));
+        }
+
+
+    }
+}
