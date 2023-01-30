f7b48f9366b429ffdcec00c96ca36a42a00360ad
Paul Catana <paulcatana97@yahoo.com>
2023-01-30 11:01:42 +0200

    Implemented Optional Class and UnitTests.

---
+++ problema 3/Problema_3/Problema_3/Optional.cs
@@ -0,0 +1,28 @@
+﻿using problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class Optional : IPattern
+    {
+        private IPattern pattern;
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (pattern.Match(text).Succes())
+            {
+                return new Match(true, text[1..]);
+            }
+
+            return new Match(true, text);
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnitTest_Optional.cs
@@ -0,0 +1,44 @@
+﻿using Problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    public class UnitTest_Optional
+    {
+        [Fact]
+        public void Match_ValidateOptionalClass_ShouldRetrunTrueAndRemainingText()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("abc");
+            Assert.Equal((true, "bc"), (match.Succes(), match.RemainingText()));
+        }
+        [Fact]
+        public void Match_ValidateOptionalClass_ShouldRetrunTrueAndText()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("bc");
+            Assert.Equal((true, "bc"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateOptionalClass_ShouldRetrunTrueAndEmptyForEmptyText()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match("");
+            Assert.Equal((true, ""), (match.Succes(), match.RemainingText()));
+        }
+        [Fact]
+        public void Match_ValidateOptionalClass_ShouldRetrunTrueAndNullForNull()
+        {
+            var a = new Optional(new Character('a'));
+            var match = a.Match(null);
+            Assert.Equal((true, null), (match.Succes(), match.RemainingText()));
+        }
+
+
+    }
+}
