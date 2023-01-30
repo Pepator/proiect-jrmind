f4389e0ec64d5772a0bf55a160652da290a1e8e6
Paul Catana <paulcatana97@yahoo.com>
2023-01-30 07:35:49 +0200

    Implemented Text class and UnitTests.

---
+++ problema 3/Problema_3/Problema_3/Text.cs
@@ -0,0 +1,28 @@
+﻿using problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class Text : IPattern
+    {
+        private string prefix;
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text) || !text.Contains(prefix))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(true, text[prefix.Length..]);
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnitTest_Text.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Problema_3
+{
+    public class UnitTest_Text
+    {
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndRemainingTextIfConditionsAreMet()
+        {
+            var b = new Text("true");
+            var match = b.Match("truex");
+            Assert.Equal((true, "x"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunfalseAndTextIfPrefixDoesNotMatch()
+        {
+            var b = new Text("true");
+            var match = b.Match("false");
+            Assert.Equal((false, "false"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndTextForEmptyPrefix()
+        {
+            var b = new Text("");
+            var match = b.Match("true");
+            Assert.Equal((true, "true"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunfalseAndNull()
+        {
+            var b = new Text("");
+            var match = b.Match(null);
+            Assert.Equal((false, null), (match.Succes(), match.RemainingText()));
+        }
+
+    }
+}
