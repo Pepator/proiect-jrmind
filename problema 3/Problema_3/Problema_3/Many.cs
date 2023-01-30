208656f1527836e00d3d1a6ead52e9ad9dc2e5ce
Paul Catana <paulcatana97@yahoo.com>
2023-01-30 10:46:07 +0200

    Implemented Many class and UnitTest_Many.

---
+++ problema 3/Problema_3/Problema_3/Many.cs
@@ -0,0 +1,31 @@
+﻿using problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class Many : IPattern
+    {
+        private IPattern pattern;
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string newText = text;
+
+            while (pattern.Match(newText).Succes())
+            {
+                newText = pattern.Match(newText).RemainingText();
+            }
+
+            return new Match(true, newText);
+
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnitTest_Many.cs
@@ -0,0 +1,52 @@
+﻿using problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class UnitTest_Many
+    {
+        [Fact]
+        public void Match_ValidateManyClass_ShouldRetrunTrueAndRemainingTextIfConditionsAreMet()
+        {
+            var a = new Many(new Character('a'));
+            var match = a.Match("aaaabc");
+            Assert.Equal((true, "bc"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndTextIfConditionsAreNotMet()
+        {
+            var a = new Many(new Character('a'));
+            var match = a.Match("bc");
+            Assert.Equal((true, "bc"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndEmptyStringForEmptyText()
+        {
+            var a = new Many(new Character('a'));
+            var match = a.Match("");
+            Assert.Equal((true, ""), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndRemainingText()
+        {
+            var digits = new Many(new problema_3.Range('0', '9'));
+            var match = digits.Match("12345ab123");
+            Assert.Equal((true, "ab123"), (match.Succes(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateTextClass_ShouldRetrunTrueAndTextIfRangeIsNotFound()
+        {
+            var digits = new Many(new problema_3.Range('0', '9'));
+            var match = digits.Match("ab");
+            Assert.Equal((true, "ab"), (match.Succes(), match.RemainingText()));
+        }
+    }
+}
