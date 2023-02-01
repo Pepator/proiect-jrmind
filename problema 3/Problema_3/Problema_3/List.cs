73af2024fee6cd4e41b279a54ccd31847076a7a5
Paul Catana <paulcatana97@yahoo.com>
2023-02-01 15:34:20 +0200

    Implemented List class and UnitTest_List

---
+++ problema 3/Problema_3/Problema_3/List.cs
@@ -0,0 +1,24 @@
+﻿using Problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    public class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Optional(new OneOrMore(new Sequence(new Optional(separator), element)));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnnitTest_List.cs
@@ -0,0 +1,69 @@
+﻿using problema_3;
+using Problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    public class UnnitTest_List
+    {
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndEmptyStringIfAllConditionsAreMet()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            var match = a.Match("1,2,3");
+            Assert.Equal((true, ""), (match.Success(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndSeparator()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            var match = a.Match("1,2,3,");
+            Assert.Equal((true, ","), (match.Success(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndRemainingText()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            var match = a.Match("1a");
+            Assert.Equal((true, "a"), (match.Success(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndText()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            var match = a.Match("abc");
+            Assert.Equal((true, "abc"), (match.Success(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndEmptyStringIfAllConditionsAreMetForComplexList()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            var match = list.Match("1; 22  ;\n 333 \t; 22");
+            Assert.Equal((true, ""), (match.Success(), match.RemainingText()));
+        }
+
+        [Fact]
+        public void Match_ValidateListClass_ShouldRetrunTrueAndAllSeparatorsForOnlyOneElement()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            var match = list.Match("1 \n;");
+            Assert.Equal((true, " \n;"), (match.Success(), match.RemainingText()));
+        }
+    }
+}
