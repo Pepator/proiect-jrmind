d7a9f386e17bcba5ebac8a57727d2dd2610aa92a
Paul Catana <paulcatana97@yahoo.com>
2023-02-02 12:21:02 +0200

    refactored Number class

--- problema 3/Problema_3/Problema_3/Number.cs
+++ problema 3/Problema_3/Problema_3/Number.cs
@@ -13,9 +13,21 @@ namespace problema_3
 
         public Number()
         {
-            pattern = new Sequence(new Sequence(new Optional(new Character('-')), new Choice(new Character('0'), new OneOrMore(new Range('0', '9')))),
-                                   new Optional(new Sequence(new Character('.'), new OneOrMore(new Range('0', '9')))),
-                                   new Optional(new Sequence(new Any("eE"), new Optional(new Any("-+")), new OneOrMore(new Range('0', '9')))));
+            var digits = new OneOrMore(new Range('0', '9'));
+
+            var integer = new Sequence(new Optional(new Character('-')),
+                                       new Choice(new Character('0'),
+                                       digits));
+
+            var fraction = new Optional(new Sequence(new Character('.'),
+                                                     digits));
+
+            var exponent = new Optional(new Sequence(new Any("eE"), 
+                                                     new Optional(new Any("-+")), 
+                                                     digits));
+
+            pattern = new Sequence(integer, fraction, exponent); 
+
         }
 
         public IMatch Match(string text)
