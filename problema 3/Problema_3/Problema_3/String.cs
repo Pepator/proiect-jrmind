6d99b8f65941d889b4c7389ecc0e00499f854250
Paul Catana <paulcatana97@yahoo.com>
2023-02-03 07:25:53 +0200

    Implemented String Class and UnitTests

---
+++ problema 3/Problema_3/Problema_3/String.cs
@@ -0,0 +1,36 @@
+﻿using Problema_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            var hex = new Choice(new Range('0', '9'),
+                                 new Range('a', 'f'),
+                                 new Range('A', 'F'));
+
+            var escape = new Choice(new Any("bfnrt\\\"/"),
+                                    new Sequence(new Character('u'), hex, hex, hex, hex));
+
+            var character = new Many(new Choice(new Range(' ', '\u0021'),
+                                       new Range('\u0023', '\u005B'),
+                                       new Range('\u005D', '\uFFFF'),
+                                       new Sequence(new Character('\\'), escape)));
+
+            pattern = new Sequence(new Character('"'), character, new Character('"'));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ problema 3/Problema_3/TestProject_3/UnitTest_String.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    public class UnitTest_String
+    {
+        [Fact]
+        public void Match_ValidateStringClass_ShouldRetrunEmptyStringForValidJsonString()
+        {
+            var a = new String();
+            var match = a.Match(Quoted(@"\""a\"" b⚾\b \f"));
+            Assert.Equal("", match.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ValidateStringClass_CanContainSolidusAndEscapedSolidus()
+        {
+            var a = new String();
+            var match = a.Match(Quoted(@"a \\ \/ b"));
+            Assert.Equal("", match.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ValidateStringClass_CanContainUnicodeCharacters()
+        {
+            var a = new String();
+            var match = a.Match(Quoted(@"a \u26Be b"));
+            Assert.Equal("", match.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ValidateStringClass_DoesNotContainUnrecognizedExcapceCharacters()
+        {
+            var a = new String();
+            var match = a.Match(Quoted(@"a\x"));
+            Assert.Equal(Quoted(@"a\x"), match.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ValidateStringClass_DoesNotEndWithReverseSolidus()
+        { 
+            var a = new String();
+            var match = a.Match(Quoted(@"a\"));
+            Assert.Equal(Quoted(@"a\"), match.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ValidateStringClass_DoesNotEndWithUnfinishedHexNumber()
+        {
+            var a = new String();
+            var match = a.Match(Quoted(@"a\u123"));
+            Assert.Equal(Quoted(@"a\u123"), match.RemainingText());
+        }
+
+        public static string Quoted(string text)
+            => $"\"{text}\"";
+    }
+}
