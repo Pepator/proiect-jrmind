1d7999ce943f72ec5d5d21ac9000e46ff22e544b
Paul Catana <paulcatana97@yahoo.com>
2023-02-06 14:24:18 +0200

    implemented Main class and Main function

---
+++ problema 3/Problema_3/Problema_3/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_3
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter text file path: ");
+
+            string text = File.ReadAllText(Console.ReadLine());
+            var value = new Value();
+            var match = value.Match(text);
+
+            if (match.Success() && match.RemainingText() == "") 
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
