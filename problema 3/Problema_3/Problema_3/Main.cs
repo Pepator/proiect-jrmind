0b14cdbec929ba0d9ce378cf6732c82b69634f0f
Paul Catana <paulcatana97@yahoo.com>
2023-02-08 12:08:07 +0200

    refactored main class

--- problema 3/Problema_3/Problema_3/Main.cs
+++ problema 3/Problema_3/Problema_3/Main.cs
@@ -8,23 +8,23 @@ namespace problema_3
 {
     class MainClass
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter text file path: ");
-
-            string text = File.ReadAllText(Console.ReadLine());
-            var value = new Value();
-            var match = value.Match(text);
-
-            if (match.Success() && match.RemainingText() == "") 
+            if (args.Length == 1)
             {
-                Console.WriteLine(true);
-            }
-            else
-            {
-                Console.WriteLine(false);
-            }
+                string text = File.ReadAllText(args[0]);
+                var value = new Value();
+                var match = value.Match(text);
 
+                if (match.Success() && match.RemainingText() == "")
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
