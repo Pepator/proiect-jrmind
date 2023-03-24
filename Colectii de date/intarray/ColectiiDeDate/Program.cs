fd6d511c72354e47a598f59f25e40afde3bfce27
Paul Catana <paulcatana97@yahoo.com>
2023-03-24 10:43:23 +0200

    implemented exeption examples

--- Colectii de date/intarray/ColectiiDeDate/Program.cs
+++ Colectii de date/intarray/ColectiiDeDate/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IntArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] studentGrades = { 10, 4, 5, 10, 7 };
+            string studentPosition = Console.ReadLine();
+
+            try
+            {
+                int position = int.Parse(studentPosition);
+                Console.WriteLine("You have selected student position : {0}", studentPosition);
+
+                try
+                {
+                    Console.WriteLine("Grade for student position {0} is {1}", position, studentGrades[position]);
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Student position not found");
+                }
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Unable to parse : {0}", studentPosition);
+                Console.WriteLine("Please enter valid position");
+            }
+        }
+    }
+}
