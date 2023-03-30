7281d1f2a619e2c69528758e04d717390bbca745
Paul Catana <paulcatana97@yahoo.com>
2023-03-30 16:16:29 +0300

    implemented ReadOnlyList and UnitTests.

---
+++ Colectii de date/intarray/ColectiiDeDate/ReadOnlyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectiiDeDate
+{
+    public class ReadOnlyList<T> : IList<T>
+    {
+        private NotSupportedException ReadOnlyExeption()
+        {
+            return new NotSupportedException("Collection is read-only.");
+        }
+
+        private readonly IList<T> readOnlyList;
+
+        public ReadOnlyList(IList<T> list)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException(Convert.ToString(list), "Array is null.");
+            }
+
+            readOnlyList = list;
+        }
+
+        public T this[int index]
+        {
+            get => readOnlyList[index];
+            set => throw ReadOnlyExeption();
+        }
+
+        public bool IsReadOnly { get => true; }
+
+        public int Count { get => readOnlyList.Count; }
+
+        public virtual void Add(T obj) => throw ReadOnlyExeption();
+
+        public int IndexOf(T obj) => readOnlyList.IndexOf(obj);
+
+        public bool Contains(T obj) => readOnlyList.Contains(obj);
+
+        public virtual void Insert(int index, T obj) => throw ReadOnlyExeption();
+
+        public void Clear() => throw ReadOnlyExeption();
+
+        public bool Remove(T obj) => throw ReadOnlyExeption();
+
+        public void RemoveAt(int index) => throw ReadOnlyExeption();
+
+        public IEnumerator<T> GetEnumerator() => readOnlyList.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => readOnlyList.GetEnumerator();
+
+        public void CopyTo(T[] array, int arrayIndex) => readOnlyList.CopyTo(array, arrayIndex);
+    }
+}
---
+++ Colectii de date/intarray/UnitTest_intarray/UnitTest_ReadOnlyList.cs
@@ -0,0 +1,81 @@
+﻿using ColectiiDeDate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectiiDeDate
+{
+    public class UnitTest_ReadOnlyList
+    {
+        [Fact]
+        public void Add_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList.Add(10));
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void IndexSetter_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList[2] = 3);
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void Insert_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList.Insert(1, 10));
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void Clear_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList.Clear());
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void Remove_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList.Remove(1));
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void RemoveAt_TestReadOnlyList_ShouldThrowNotSupportedExection()
+        {
+            var testList = new SortedList<int> { 4, 2, 1, 3 };
+            var readOnlyList = new ReadOnlyList<int>(testList);
+
+            var exception = Assert.Throws<NotSupportedException>(() => readOnlyList.RemoveAt(1));
+            Assert.Equal("Collection is read-only.", exception.Message);
+        }
+
+        [Fact]
+        public void NullExeption_TestReadOnlyList_ShouldThrowNullExection()
+        {
+            ReadOnlyList<int> readOnlyList;
+
+            var exception = Assert.Throws<ArgumentNullException>(() => readOnlyList = new ReadOnlyList<int>(null));
+            Assert.Equal("Array is null.", exception.Message);
+        }
+    }
+}
