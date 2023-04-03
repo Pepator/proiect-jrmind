df4d798e0b44a7e59650b267c5269cdb91cc9ff8
Paul Catana <paulcatana97@yahoo.com>
2023-04-03 16:10:11 +0300

    implemented Stream class and UnitTest

---
+++ Colectii de date/intarray/ColectiiDeDate/Stream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectiiDeDate
+{
+    public class Stream
+    {
+        public static void Write(string text)
+        {
+            StreamWriter writer = new StreamWriter(@"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt");
+            writer.Write(text);
+            writer.Close();
+        }
+
+        public static void Read(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            reader.ReadToEnd();
+            reader.Close();
+        }
+    }
+}
---
+++ Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectiiDeDate
+{
+    public class UnitTest_Stream
+    {
+        [Fact]
+        public void Stream_ShouldWriteAndReadFromTextFile()
+        {
+            Stream.Write("Hello World!");
+            Stream.Read(@"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt");
+            string textFromFile = File.ReadAllText(@"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt");
+            Assert.Equal("Hello World!", textFromFile);
+        }
+    }
+}
