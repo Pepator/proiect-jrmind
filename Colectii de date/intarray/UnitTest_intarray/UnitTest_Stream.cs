8048d5a93933c681f9e7be350ef7c2e1bc5a0ccd
Paul Catana <paulcatana97@yahoo.com>
2023-04-04 10:51:26 +0300

    changed absolute path to relative path for Stream UnitTests

--- Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
+++ Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
@@ -12,27 +12,25 @@ namespace ColectiiDeDate
         public void StreamWrite_ShouldWriteinTextFile()
         {
             string myText = "Hello World!";
-            string filePath = @"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt";
+            string filePath = "Text.txt";
 
             FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
 
             StreamClass.Write(fs, myText);
 
-            string textFromFile = File.ReadAllText(@"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt");
+            string textFromFile = File.ReadAllText("Text.txt");
             Assert.Equal("Hello World!", textFromFile);
         }
 
         [Fact]
         public void StreamRead_ShouldWriteAndReadFromTextFile()
         {
-            string myText = "Hello World!";
-            string filePath = @"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt";
+            string filePath = "Text.txt";
 
             FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
 
             string textFromFile = StreamClass.Read(fs);
 
-            //string textFromFile = File.ReadAllText(@"D:\github\Colectii de date\intarray\ColectiiDeDate\Text.txt");
             Assert.Equal("Hello World!", textFromFile);
         }
     }
