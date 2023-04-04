441a4441215250d10b1374032944d0ca37834f8e
Paul Catana <paulcatana97@yahoo.com>
2023-04-04 11:50:13 +0300

    refactored Stream tests to use MemoryStream instead of text.txt

--- Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
+++ Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
@@ -9,28 +9,31 @@ namespace ColectiiDeDate
     public class UnitTest_Stream
     {
         [Fact]
-        public void StreamWrite_ShouldWriteinTextFile()
+        public void StreamWrite_ShouldWriteInMemoryStream()
         {
             string myText = "Hello World!";
-            string filePath = "Text.txt";
 
-            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+            MemoryStream memoryStream = new MemoryStream();
 
-            StreamClass.Write(fs, myText);
+            StreamClass.Write(memoryStream, myText);
+            memoryStream = new MemoryStream(memoryStream.ToArray());
 
-            string textFromFile = File.ReadAllText("Text.txt");
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            byte[] bytesFromFile = new byte[myText.Length];
+            memoryStream.Read(bytesFromFile, 0, bytesFromFile.Length);
+            string textFromFile = Encoding.UTF8.GetString(bytesFromFile);
             Assert.Equal("Hello World!", textFromFile);
         }
 
         [Fact]
-        public void StreamRead_ShouldWriteAndReadFromTextFile()
+        public void StreamRead_ShouldReadFromMemoryStream()
         {
-            string filePath = "Text.txt";
+            string myText = "Hello World!";
+            byte[] byteText = Encoding.UTF8.GetBytes(myText);
 
-            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+            MemoryStream memoryStream = new MemoryStream(byteText);
 
-            string textFromFile = StreamClass.Read(fs);
-
+            string textFromFile = StreamClass.Read(memoryStream);
             Assert.Equal("Hello World!", textFromFile);
         }
     }
