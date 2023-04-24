d8f4e283f1d74b08ecd6b2724bafa280fdffecdb
Paul Catana <paulcatana97@yahoo.com>
2023-04-24 13:00:57 +0300

    encrypt and decrypt message in MemoryStream

--- Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
+++ Colectii de date/intarray/UnitTest_intarray/UnitTest_Stream.cs
@@ -9,32 +9,17 @@ namespace ColectiiDeDate
     public class UnitTest_Stream
     {
         [Fact]
-        public void StreamWrite_ShouldWriteInMemoryStream()
+        public void StreamWrite_ShouldWriteInMemoryStreamAsync()
         {
             string myText = "Hello World!";
-
             MemoryStream memoryStream = new MemoryStream();
-
             StreamClass.Write(memoryStream, myText);
             memoryStream = new MemoryStream(memoryStream.ToArray());
-
             memoryStream.Seek(0, SeekOrigin.Begin);
-            byte[] bytesFromFile = new byte[myText.Length];
-            memoryStream.Read(bytesFromFile, 0, bytesFromFile.Length);
-            string textFromFile = Encoding.UTF8.GetString(bytesFromFile);
-            Assert.Equal("Hello World!", textFromFile);
-        }
 
-        [Fact]
-        public void StreamRead_ShouldReadFromMemoryStream()
-        {
-            string myText = "Hello World!";
-            byte[] byteText = Encoding.UTF8.GetBytes(myText);
+            string textFromStream = StreamClass.Read(memoryStream);
 
-            MemoryStream memoryStream = new MemoryStream(byteText);
-
-            string textFromFile = StreamClass.Read(memoryStream);
-            Assert.Equal("Hello World!", textFromFile);
+            Assert.Equal("Hello World!", textFromStream);
         }
     }
 }
